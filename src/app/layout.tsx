40aece29d6c07a23eda912cca66892a2aedbbdb8
takam-alex-christian <takamalexchristian@gmail.com>
2025-01-29 11:23:08 +0800

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import PageFooter from "@/components/PageFooter";
 
 const PoppinsFont = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
